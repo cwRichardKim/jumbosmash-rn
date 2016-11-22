84dad94e93e537ede030754e882145b057d2ef00
cwrichardkim <cwrichardkim@gmail.com>
2016-11-22 14:43:45 -0500

    swiping UI works, setup architecture for login and chat

---
+++ components/cards/noMoreCards.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*
+This file is responsible for the view when there are no more cards left
+*/
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+
+class NoMoreCards extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.noMoreCardsText}>No more cards</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  noMoreCardsText: {
+    fontSize: 22,
+  }
+})
+
+export default NoMoreCards;
--- index.ios.js
+++ index.ios.js
@@ -4,28 +4,82 @@
  * @flow
  */
 
+import * as firebase from 'firebase';
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
+  TouchableHighlight,
 } from 'react-native';
+
+import SwipingPage from "./components/cards/swipingPage.js";
+import ChatPage    from "./components/chat/chatPage.js";
+import LoginPage   from "./components/login/loginPage.js";
+
+const firebaseConfig = { //TODO: add real keys @elif, with environment variables?
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  storageBucket: "<your-storage-bucket>",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class jumbosmash extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <NavigatorIOS
+        initialRoute={{
+          component: TemporaryMenu,
+          title: 'Temporary Menu',
+        }}
+        style={{flex:1}}
+      />
+    );
+  }
+}
+
+// TEMPORARY, will remove once we get a menu design
+class TemporaryMenu extends Component {
+  _toSwipingPage = () => {
+    this.props.navigator.push({
+      component: SwipingPage,
+      title: 'Swiping',
+    });
+  }
+
+  _toChatPage = () => {
+    this.props.navigator.push({
+      component: ChatPage,
+      title: 'Chat',
+    });
+  }
+
+  _toLoginPage = () => {
+    this.props.navigator.push({
+      component: LoginPage,
+      title: 'Login',
+    });
+  }
+
+  render() {
+    return (
+      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <Text>We'll get rid of this once we get an actual menu design {'\n'}</Text>
+        <TouchableHighlight onPress={this._toSwipingPage}>
+          <Text>Go to swiping</Text>
+        </TouchableHighlight>
+        <Text>{'\n'}</Text>
+        <TouchableHighlight onPress={this._toChatPage}>
+          <Text>Go to chat</Text>
+        </TouchableHighlight>
+        <Text>{'\n'}</Text>
+        <TouchableHighlight onPress={this._toLoginPage}>
+          <Text>Go to login</Text>
+        </TouchableHighlight>
       </View>
     );
   }
