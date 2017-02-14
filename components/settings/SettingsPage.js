6ee47f181a6de97757e49943017c3abb1ed65323
cwrichardkim <cwrichardkim@gmail.com>
2017-02-13 23:53:43 -0500

    starting settings UI

--- components/settings/SettingsPage.js
+++ components/settings/SettingsPage.js
@@ -10,25 +10,23 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
 } from 'react-native';
+
+import ProfilePhotoPicker from "./ProfilePhotoPicker.js"
 
 class SettingsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photos: props.photos ? props.photos : [],
-      firstName: props.firstName ? props.firstName : "",
-      lastName: props.lastName ? props.lastName : "",
-      description: props.description ? props.description : "",
-      major: props.major ? props.major : "",
     }
   }
 
   render() {
     return (
-      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-        <Text>Soon to be Settings</Text>
-      </View>
+      <ScrollView>
+        <ProfilePhotoPicker photos={this.props.profile.photos}/>
+      </ScrollView>
     );
   }
 }
