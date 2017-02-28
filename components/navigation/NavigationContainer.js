b58fb1d7fe3d7f07717c32b24328312749b4c08c
cwrichardkim <cwrichardkim@gmail.com>
2017-02-28 18:10:17 -0500

    removed deck view and refactored the notification code to make sense. also added drag animations WIP

--- components/navigation/NavigationContainer.js
+++ components/navigation/NavigationContainer.js
@@ -12,7 +12,6 @@ import {
   View,
   Image,
   TabBarIOS,
-  Animated,
   TouchableHighlight,
   Navigator,
   Alert,
@@ -24,6 +23,7 @@ import NotificationBannerView from "./NotificationBannerView.js"
 import ChatPage               from "../chat/ChatPage.js"
 import ConversationPage       from "../chat/ConversationPage.js"
 const global = require('../global/GlobalFunctions.js');
+const TabNames = global.tabNames();
 const StorageKeys = global.storageKeys();
 
 const firebase = require('firebase');
@@ -51,9 +51,8 @@ class NavigationContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTab: global.tabNames().cardsTab,
+      selectedTab: TabNames.cardsTab,
       notificationBannerText: "notification (tap me, goes to chat)",
-      pan: new Animated.ValueXY({x:0, y:-100}),
       profiles: [],
       myProfile: testProfile,
       hasUnsavedSettings: false,
@@ -61,11 +60,10 @@ class NavigationContainer extends Component {
   }
 
   componentDidMount() {
-    // this._showNotificationBanner();
     this._shouldRetrieveProfilesFromStorage();
   }
 
-  // Called when the app is closed from DeckView.js
+  // Called when the app is closed from SwipingPage.js
   // Removes all the old cards and saves the remainder to AsyncStorage
   _removeSeenCards(currentIndex) {
     let oldLength = this.state.profiles.length;
@@ -163,38 +161,13 @@ class NavigationContainer extends Component {
       });
   }
 
-  _hideNotificationBanner() {
-    Animated.spring(
-      this.state.pan,
-      {
-        toValue: {x:0, y:-100},
-      }
-    ).start();
-  }
-
-  _showNotificationBanner() {
-    Animated.spring(
-      this.state.pan,
-      {
-        toValue: {x:0, y:-25},
-      }
-    ).start();
-  }
-
-  _notificationBannerTapped() {
-    this._hideNotificationBanner();
-    this.setState({
-      selectedTab: 'chatTab',
-    });
-  }
-
   // Changes which tab is showing (swiping, settings, etc), check HomeTabBarIOS
   // for the tab names.  The reason why this is here is because notifications
   // will also need to change the tabs, and changes can only trickle downwards.
   // Thus, selectedTab is a property of the Navigator, and TabBar looks to
   // Navigator for this property.
   _changeTab(tabName) {
-    const settingsTab = global.tabNames().settingsTab;
+    const settingsTab = TabNames.settingsTab;
     let currentlyOnSettings = this.state.selectedTab == settingsTab;
     let leavingSettings = currentlyOnSettings && tabName != settingsTab;
     if (leavingSettings && this.state.hasUnsavedSettings) {
@@ -265,13 +238,14 @@ class NavigationContainer extends Component {
             }}
             removeSeenCards={this._removeSeenCards.bind(this)}
           />
-          <Animated.View
-            style={[styles.notificationBanner, {transform:this.state.pan.getTranslateTransform()}]}>
-            <NotificationBannerView
-              message={this.state.notificationBannerText}
-              onPress={this._notificationBannerTapped.bind(this)}
-            />
-          </Animated.View>
+          <NotificationBannerView
+            style={styles.notificationBanner}
+            message={this.state.notificationBannerText}
+            onPress={()=>{
+              console.log("asdfasdfasdf");
+              this._changeTab(TabNames.chatTab);
+            }}
+          />
         </View>
       );
     } else if (route.name == 'Chat') {
@@ -298,7 +272,7 @@ class NavigationContainer extends Component {
 const styles = StyleSheet.create({
   notificationBanner: {
     position: 'absolute',
-    height: 100,
+    height: 200,
     top: 0,
     left: 0,
     right: 0,
--- components/navigation/NotificationBannerView.js
+++ components/navigation/NotificationBannerView.js
@@ -10,19 +10,94 @@ import {
   Text,
   View,
   TouchableHighlight,
+  Animated,
+  PanResponder
 } from 'react-native';
+
+const VERTICAL_THRESHOLD = 10;
+const BANNER_SHOW_HEIGHT = 75;
+const BANNER_TOTAL_HEIGHT = 200; // from NavigationContainer
+
+const INITIAL_POSITION = {x:0, y: -BANNER_TOTAL_HEIGHT};
+const SHOW_POSITION = {x:0, y: BANNER_SHOW_HEIGHT - BANNER_TOTAL_HEIGHT};
+import clamp from 'clamp';
 
 class NotificationBannerView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      pan: new Animated.ValueXY(INITIAL_POSITION),
+    }
   }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
+        return gestureState.dy != 0;
+      },
+
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setOffset({x: 0, y: this.state.pan.y._value});
+        this.state.pan.setValue({x: 0, y: 0});
+      },
+
+      onPanResponderMove: Animated.event([
+        null, {dx: 0, dy: this.state.pan.y},
+      ]),
+
+      onPanResponderRelease: (e, {vx, vy}) => {
+        this.state.pan.flattenOffset();
+
+        console.log(Math.abs(this.state.pan.y._value - SHOW_POSITION.y))
+        if (Math.abs(this.state.pan.y._value - SHOW_POSITION.y) > VERTICAL_THRESHOLD) {
+          let yvelocity =  clamp(vy, -3, 3);
+
+          this._hideNotificationBanner();
+        } else { // return back
+          this._showNotificationBanner();
+        }
+      }
+    })
+  }
+
+  componentDidMount () {
+    this._showNotificationBanner();
+  }
+
+  _showNotificationBanner(callback) {
+    Animated.spring(
+      this.state.pan,
+      {
+        toValue: SHOW_POSITION,
+      }
+    ).start(callback);
+  }
+
+  _hideNotificationBanner(callback) {
+    Animated.spring(
+      this.state.pan,
+      {
+        toValue: INITIAL_POSITION,
+      }
+    ).start(callback);
+  }
+
+  _notificationBannerTapped() {
+    this._hideNotificationBanner(this.props.onPress());
+  }
+
   render() {
     return(
-      <TouchableHighlight style={styles.container} onPress={this.props.onPress}>
-        <View style={[styles.view]}>
-          <Text style={styles.text}>{this.props.message ? this.props.message : "test"}</Text>
-        </View>
-      </TouchableHighlight>
+      <Animated.View
+        style={[this.props.style, {transform:this.state.pan.getTranslateTransform()}]}
+        {...this._panResponder.panHandlers}>
+        <TouchableHighlight style={styles.container} onPress={this._notificationBannerTapped.bind(this)}>
+          <View style={[styles.view]}>
+            <Text style={styles.text}>{this.props.message ? this.props.message : "test"}</Text>
+          </View>
+        </TouchableHighlight>
+      </Animated.View>
     );
   }
 }
@@ -38,7 +113,7 @@ const styles = StyleSheet.create({
     backgroundColor: "gray"
   },
   text: {
-    paddingTop: 25,
+    paddingTop: BANNER_TOTAL_HEIGHT - BANNER_SHOW_HEIGHT,
     fontSize: 16,
     color: 'white',
     backgroundColor: 'transparent',
