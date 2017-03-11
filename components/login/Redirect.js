e73be8f7ed55e0e0358c9550bf03fc6c253a6896
Jade <jadeyychan@gmail.com>
2017-03-11 16:51:12 -0500

    validates that user is in existing student database

--- components/login/Redirect.js
+++ components/login/Redirect.js
@@ -19,6 +19,28 @@ import SignupPage             from './SignupPage.js';
 
 class Redirect extends Component {
 
+  updatesLoginStatus() {
+    this.props.firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+
+        // user is signed in
+        var email = user.email;
+        user.getToken()
+          .then((accessToken) => {
+            Alert.alert("access token", accessToken);
+            this.goToAccountPage();
+          })
+          .catch((error) => {
+            Alert.alert("there's been an error getting the token?!"); 
+          })
+      } else {
+        // user is not logged in
+        Alert.alert("you're not logged in");
+        this.goToSignupPage();
+      }
+    });
+  }
+
   static goToLoginPage(){
     this.props.navigator.push({
       component: LoginPage
