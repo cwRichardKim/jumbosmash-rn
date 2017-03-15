b806bb9856326a65b30d445820252de80f4db7b2
Jared Moskowitz <jaredmoskowitz@me.com>
2017-03-14 21:13:46 -0400

    initial workings of notifications

--- components/chat/ChatPage.js
+++ components/chat/ChatPage.js
@@ -42,42 +42,6 @@ class ChatPage extends React.Component {
     if (_listView) {
       _listView.scrollTo({x: 0, y: SCROLL_TO_Y, animated: true});
     }
-
-    let PushNotification = require('react-native-push-notification');
-
-    PushNotification.configure({
-
-        // (optional) Called when Token is generated (iOS and Android)
-        onRegister: function(token) {
-            console.log( 'TOKEN:', token );
-        },
-
-        // (required) Called when a remote or local notification is opened or received
-        onNotification: function(notification) {
-            console.log( 'NOTIFICATION:', notification );
-        },
-
-        // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
-        senderID: "YOUR GCM SENDER ID",
-
-        // IOS ONLY (optional): default: all - Permissions to register.
-        permissions: {
-            alert: true,
-            badge: true,
-            sound: true
-        },
-
-        // Should the initial notification be popped automatically
-        // default: true
-        popInitialNotification: true,
-
-        /**
-          * (optional) default: true
-          * - Specified if permissions (ios) and token (android and ios) will requested or not,
-          * - if not, you must call PushNotificationsHandler.requestPermissions() later
-          */
-        requestPermissions: true,
-    });
   }
 
   _fetchConversationsAsync () {
