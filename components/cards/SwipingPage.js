e098336d4b45c83745df151309921b47bc49b0c0
Jared Moskowitz <jaredmoskowitz@me.com>
2017-03-14 23:53:25 -0400

    swiping and token gotten correctly

--- components/cards/SwipingPage.js
+++ components/cards/SwipingPage.js
@@ -81,7 +81,7 @@ class SwipingPage extends Component {
   }
 
   async _asyncUpdateLikeList(profId, swipeId) {
-    let url = "https://jumbosmash2017.herokuapp.com/profile/like/".concat(profId).concat("/").concat(swipeId).concat("/").concat(this.props.token);
+    let url = "https://jumbosmash2017.herokuapp.com/profile/like/".concat(profId).concat("/").concat(swipeId).concat("/").concat(this.props.token.val);
     fetch(url, {
       method: 'POST',
       headers: {
