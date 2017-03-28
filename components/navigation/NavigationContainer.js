33eb9d710ad3d07e00bf8207b7ff1b7a3aafba9c
cwrichardkim <cwrichardkim@gmail.com>
2017-03-28 17:20:33 -0400

    fixed responsejson bug

--- components/navigation/NavigationContainer.js
+++ components/navigation/NavigationContainer.js
@@ -134,15 +134,15 @@ class NavigationContainer extends Component {
 
           // data is not of the correct type
         } else {
-          this._fetchProfiles(0, FIRST_BATCH_SIZE);
+          this._fetchProfiles(FIRST_BATCH_SIZE);
         }
         // storage is null / empty
       } else {
-        this._fetchProfiles(0, FIRST_BATCH_SIZE);
+        this._fetchProfiles(FIRST_BATCH_SIZE);
       }
       // error accessing storage
     } catch (error) {
-      this._fetchProfiles(0, FIRST_BATCH_SIZE);
+      this._fetchProfiles(FIRST_BATCH_SIZE);
       throw error;
     }
   }
@@ -174,38 +174,41 @@ class NavigationContainer extends Component {
   // fetches new profiles and adds them to the profiles array
   // lastID: the lastID we got from the previous list of profiles
   // count: how many profiles to fetch. 0 or null is all
-  async _fetchProfiles(lastID, count) {
+  async _fetchProfiles(count) {
     let index = await this._getLastIndex();
-    // console.log("request made with last index: "+index.toString()); //TODO @richard testing code remove
     let id = this.props.myProfile.id.toString();
     let batch = count ? count.toString() : FETCH_BATCH_SIZE.toString();
     let url = "https://jumbosmash2017.herokuapp.com/profile/batch/"+id+"/"+index+"/"+batch+"/"+this.token.val;
     console.log("Fetching profiles for "+id+" batch size: " + batch + " index: " + index);
     return fetch(url)
     .then((response) => {
-      if ("status" in response && response["status"] >= 200 && response["status"] < 300) {
+      if (global.isGoodResponse(response)) {
         return response.json();
       } else {
         throw ("status" in response) ? response["status"] : "Unknown Error";
       }
     }).then((responseJson) => {
-      if (__DEV__) { //TODO @richard remove. for debugging purposes
-        this.navigator.notificationBanner.showWithMessage("Retrieved " + responseJson.length + " profiles. prev index: "+index+", indexes: " + responseJson[0].index.toString() + " - " + responseJson[responseJson.length-1].index.toString())
-      }
       if (responseJson.length > 0) {
+        if (__DEV__) { //TODO @richard remove. for debugging purposes
+          this.navigator.notificationBanner.showWithMessage("Retrieved " + responseJson.length + " profiles. prev index: "+index+", indexes: " + responseJson[0].index.toString() + " - " + responseJson[responseJson.length-1].index.toString())
+        }
         this._setLastIndex(responseJson[responseJson.length - 1].index);
+        // for (var i = 0; i < responseJson.length; i++) { //TODO @richard testing code remove
+        //   console.log(responseJson[i].index.toString() + " " + responseJson[i].firstName);
+        // }
+        global.shuffle(responseJson);
+        this.setState({
+          profiles: this.state.profiles.concat(responseJson),
+        })
       } else {
-        //TODO @richard test this
-        console.log("on fetching new profiles, we hit a case where nothing was returned (likely due to this being the last index)")
-        this._setLastIndex(0);
+        if (this._getLastIndex() === 0) {
+          // we're in a special case where the user has swiped right on everyone or
+          //TODO @richard notify the user
+        } else {
+          this._setLastIndex(0);
+          this._fetchProfiles(FETCH_BATCH_SIZE);
+        }
       }
-      // for (var i = 0; i < responseJson.length; i++) { //TODO @richard testing code remove
-      //   console.log(responseJson[i].index.toString() + " " + responseJson[i].firstName);
-      // }
-      global.shuffle(responseJson);
-      this.setState({
-        profiles: this.state.profiles.concat(responseJson),
-      })
     })
     .catch((error) => {
       //TODO: @richard replace with real catch case
@@ -244,7 +247,7 @@ class NavigationContainer extends Component {
     //TODO: @richard this is temporary while the backend isn't up yet
     let newProfile = {};
     for (let key in this.props.myProfile) {
-      newProfile[key] = (key in profileChanges) ? profileChanges[key] : this.props.myProfile[key];
+      newProfile[key] = this.props.myProfile[key];
     }
     for (let key in profileChanges) {
       newProfile[key] = profileChanges[key];
