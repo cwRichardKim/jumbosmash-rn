2fb5ac2243a85a0ca7542979fe14ff2da0b963be
cwrichardkim <cwrichardkim@gmail.com>
2017-03-29 14:00:11 -0400

    pre-release button only shows up when needed

--- components/misc/PreReleasePage.js
+++ components/misc/PreReleasePage.js
@@ -17,6 +17,7 @@ import {
 import GlobalStyles           from "../global/GlobalStyles.js";
 import GlobalFunctions        from "../global/GlobalFunctions.js"
 import RectButton             from "../global/RectButton.js";
+let Mailer = require('NativeModules').RNMail;
 const OverrideActions = GlobalFunctions.overrideActions();
 const HEIGHT = Dimensions.get('window').height;
 const WIDTH = Dimensions.get('window').width;
@@ -42,7 +43,7 @@ class PreReleasePage extends Component {
     }
   }
 
-  _loadPrereleaseApp() {
+  _loadPreReleaseApp() {
     Alert.alert(
       "Opening app in pre-release state",
       "Jumbosmash will be released on May 12th, 2017.\n\nOnly developers and beta testers have access to the app before then. In this pre-release state, the app will function normally, but some features might seem empty. For example, no chats or matches will exist since no user will have swiped right on you.\n\nteam@jumbosmash.com for questions!",
@@ -50,6 +51,52 @@ class PreReleasePage extends Component {
         {text:"Open in Pre-release State", onPress:()=>{this.props.changePage(OverrideActions.openApp)}},
       ]
     )
+  }
+
+  _sendMail() {
+    if (Mailer && Mailer.mail) {
+      Mailer.mail({
+        subject: 'Requesting Early Access',
+        recipients: ['team@jumbosmash.com'],
+        body: '[account email: ]',
+      }, (error, event) => {
+        if(error) {
+          Alert.alert('Error', 'Could not send mail. Try sending an email to team@jumbosmash.com through your mail client');
+        }
+      });
+    } else {
+      Alert.alert(
+        "Unsupported Device",
+        "Sorry, your device doesn't support in-app email :(\nSend your question / feedback to team@jumbosmash.com with your mail client",
+        [{text:"OK", onPress:()=>{}}]
+      )
+    }
+  }
+
+  _shouldLoadPreReleaseButton () {
+    if (this.props.myProfile && (this.props.myProfile.teamMember === true || this.props.myProfile.betaTester === true)) {
+      return (
+        <View style={styles.buttonContainer}>
+          <RectButton
+            style={[styles.button, styles.smashButton]}
+            textStyle={styles.buttonText}
+            text="PRE-RELEASE"
+            onPress={this._loadPreReleaseApp.bind(this)}
+          />
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.buttonContainer}>
+          <RectButton
+            style={[styles.button, styles.smashButton]}
+            textStyle={styles.buttonText}
+            text="Request Early Access"
+            onPress={this._sendMail.bind(this)}
+          />
+        </View>
+      )
+    }
   }
 
   render() {
@@ -60,17 +107,10 @@ class PreReleasePage extends Component {
             <Text style={[GlobalStyles.boldText, styles.title]}>Welcome to Jumbosmash ;)</Text>
           </View>
           <View style={styles.textContainer}>
-            <Text style={[GlobalStyles.boldText, {marginBottom: 10}]}>We hope you had fun</Text>
-            <Text style={GlobalStyles.text}>Using JumboSmash, we certainly had fun making it. We wish you all the best of luck and hope you keep in contact with your matches. Off to the real world we go!</Text>
+            <Text style={[GlobalStyles.boldText, {marginBottom: 10}]}>Only X more days until release</Text>
+            <Text style={GlobalStyles.text}>text etc etc</Text>
           </View>
-          <View style={styles.buttonContainer}>
-            <RectButton
-              style={[styles.button, styles.smashButton]}
-              textStyle={styles.buttonText}
-              text="PRE-RELEASE"
-              onPress={this._loadPrereleaseApp.bind(this)}
-            />
-          </View>
+          {this._shouldLoadPreReleaseButton()}
         </ScrollView>
       </View>
     )
