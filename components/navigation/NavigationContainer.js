f09dd36d8c26c997b18142b440f5017636e5533a
cwrichardkim <cwrichardkim@gmail.com>
2017-04-03 17:06:54 -0400

    first steps to android

--- components/navigation/NavigationContainer.js
+++ components/navigation/NavigationContainer.js
@@ -52,6 +52,7 @@ class NavigationContainer extends Component {
           console.log(error);
         });
     }
+    console.log("NAVIGATION");
     AppState.addEventListener('change', this._handleAppStateChange.bind(this));
   }
 
