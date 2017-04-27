8b0f453b04e6685ef480c9ff8441ab9888a12f74
Jared Moskowitz <jaredmoskowitz@me.com>
2017-04-27 00:21:13 -0400

    android notifications working

--- components/navigation/JumboNavigator.js
+++ components/navigation/JumboNavigator.js
@@ -104,7 +104,13 @@ class JumboNavigator extends Component {
 
         // (required) Called when a remote or local notification is opened or received
         onNotification: function(notification) {
-            PushNotifications.onNotification(notification,
+            let message = "";
+            if (Platform.OS === 'android') {
+              message = notification.message;
+            } else {
+              message = notification.message.body;
+            }
+            PushNotifications.onNotification(message,
               {banner: this.notificationBanner,
                onPress: () => {this.changePage(PageNames.chatPage)},
                firebase: this.props.firebase,
